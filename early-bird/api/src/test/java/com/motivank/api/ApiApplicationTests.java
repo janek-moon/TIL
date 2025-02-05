90f8d45417ff8520fea3da28a8fe580f6bd2d67f
EunhoMoon <eunho.moon@gmail.com>
2025-02-05 11:43:56 +0900

    [선착순 이벤트] init project

---
+++ early-bird/api/src/test/java/com/motivank/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

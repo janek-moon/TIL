60bce69a3c8f46452b0b5f34ee9335450677a9d4
Janek <eunho.moon@gmail.com>
2025-10-29 21:16:59 +0900

    [kcu] 컴퓨터 프로그래밍 1 ~ 3주차

---
+++ khu/computer-programming/week-03/1-variable.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	printf("변수 x의 크기:      %d\n", sizeof(x)); // 4
+
+	printf("char형의 크기:      %d\n", sizeof(char)); // 1
+	printf("int형의 크기:       %d\n", sizeof(int));  // 4
+	printf("short형의 크기:     %d\n", sizeof(short));// 2
+	printf("long형의 크기:      %d\n", sizeof(long)); // 8
+	printf("long long형의 크기: %d\n", sizeof(long long)); // 8
+	printf("float형의 크기:     %d\n", sizeof(float));	// 4
+	printf("double형의 크기:    %d\n", sizeof(double));	// 8
+
+	return 0;
+}
